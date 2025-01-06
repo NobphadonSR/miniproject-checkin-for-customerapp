8aa1b8a6e9f2cc41265bcaf981be5877f93435a7
PunznyNobphadon <nobphadon.sr.64@ubu.ac.th>
2025-01-06 11:21:12 +0700

    อัพเดทของมูลเพิ่มเติมสำหรับ build.sh และ settings.py

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# รันเฉพาะครั้งแรกที่ deploy หรือเมื่อมีการเปลี่ยนแปลง model
+python manage.py makemigrations
 python manage.py migrate
+
+python manage.py collectstatic --no-input
--- checkin/settings.py
+++ checkin/settings.py
@@ -15,6 +15,25 @@ from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
+if 'RENDER' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,15 +119,6 @@ WSGI_APPLICATION = 'checkin.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://' + os.getenv('DB_USER', '') + ':' + os.getenv('DB_PASSWORD', '') + '@' + os.getenv('DB_HOST', '') + ':' + os.getenv('DB_PORT', '5432') + '/' + os.getenv('DB_NAME', ''),
-        conn_max_age=600
-    )
-}
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
